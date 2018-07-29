c3f6d51e1d2e5f41148090f3bdaf8d1b516388a3
Hayley Musgrave <hayleymusgrave@hotmail.com>
2018-07-29 22:05:22 +1200

    Working on the death animation, mid-progress

--- Assets/scripts/bunnyController.cs
+++ Assets/scripts/bunnyController.cs
@@ -1,26 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class bunnyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
     private Animator myAnim;
     public float bunnyJumpForce = 500f;
-
-	// Use this for initialization
+    private float bunnyHurtTime = -1;
+    
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonUp("Jump"))
+        if (bunnyHurtTime == -1)
         {
-            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            if (Input.GetButtonUp("Jump"))
+            {
+                myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            }
+            myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
         }
-
-        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+        else
+        {
+            if (Time.time > bunnyHurtTime + 2)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            
+        }
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            bunnyHurtTime = Time.time;
+            myAnim.SetBool("bunnyHurt", true);
+        }
+    }
 }
