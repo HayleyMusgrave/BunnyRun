9fc8a84426abfdb37f4290193ad351372fe7ab93
DFDragoness <37249609+DFDragoness@users.noreply.github.com>
2018-08-04 09:27:34 +1200

    Tweaks and added mute button

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    public GameObject audioOnIcon;
+    public GameObject audioOffIcon;
+
     void Start()
     {
-
+        SetSoundState();
     }
 
     void Update()
@@ -22,5 +25,34 @@ public class MenuController : MonoBehaviour {
     public void StartGame()
     {
         SceneManager.LoadScene("BaseScene");
+    }
+
+    public void ToggleSound()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+        }
+        SetSoundState();
+    }
+
+    private void SetSoundState()
+    {
+        if (PlayerPrefs.GetInt("Muted",0) == 0)
+        {
+            AudioListener.volume = 1;
+            audioOnIcon.SetActive(true);
+            audioOffIcon.SetActive(false);
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            audioOnIcon.SetActive(false);
+            audioOffIcon.SetActive(true);
+        }
     }
 }
